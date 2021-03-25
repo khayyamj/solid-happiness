09e627a61b08ffde4d2a2f9955f0b956aee01e7a
Khayyam Jones <khayyam.jones@stgconsulting.com>
2021-03-25 09:26:11 -0600

    completed navigation with drawer nav

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react"
 import { Text, View } from "react-native"
 import * as Font from "expo-font"
-import AppLoading from "expo-app-loading"
-import MealsNavigator from "./navigation/MealsNavigator"
+import { AppLoading } from "expo"
 import { enableScreens } from "react-native-screens"
+
+import MealsNavigator from "./navigation/MealsNavigator"
 
 enableScreens()
 
 const fetchFonts = () => {
-  Font.loadAsync({
+  return Font.loadAsync({
     "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
     "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
   })
@@ -16,19 +17,15 @@ const fetchFonts = () => {
 
 export default function App() {
   const [fontLoaded, setFontLoaded] = useState(false)
-  console.log(
-    "%c fontLoaded ->",
-    "background: black; color: yellow; font-size: 11px",
-    fontLoaded
-  )
+
   if (!fontLoaded) {
     return (
       <AppLoading
         startAsync={fetchFonts}
         onFinish={() => setFontLoaded(true)}
-        onError={() => console.log("unable to load fonts...")}
       />
     )
   }
+
   return <MealsNavigator />
 }
